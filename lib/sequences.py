0bee42cbd6e32e2afed563267438d3fef67a87ee
Brian <brian.kiprono8@student.moringaschool.com>
2025-05-09 07:09:33 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    count = 0
+    result = []
+
+    while count < length:
+        result += [a]  
+        a, b = b, a + b
+        count += 1
+
+    print(result)
